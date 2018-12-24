cf1831ef36ef2dd7117635182660d8ea9401296e
Serhii Kulik <sergey15ne@gmail.com>
2018-12-24 21:16:20 +0200

    comm

---
+++ dev/README.md
@@ -0,0 +1,36 @@
+# testtask
+
+> 
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+
+# build for production and view the bundle analyzer report
+npm run build --report
+```
+
+For detailed explanation on how things work, checkout the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
+
+### StyleLint usage
+
+``` bash
+# install stylelint package globaly
+npm install stylelint --global
+```
+Enable stylelint plugin and set "Stylelint package" path
+``` bash
+Default Settings -> Languages & Frameworks -> Stylesheets -> Stylelint
+```
+Stylelint config:
+```text
+/dev/.stylelint
+```
---
+++ dev/src/App.vue
@@ -0,0 +1,83 @@
+<template>
+    <Resize id="app" class="app-layout">
+        <div class="container">
+
+            <menu-app></menu-app>
+
+            <div class="center">
+                <router-view></router-view>
+            </div>
+
+
+            <ul class="social-list">
+                <li>
+                    <share-link network="fb" url="" class="icon">
+                        <fb></fb>
+                    </share-link>
+                </li>
+                <li>
+                    <share-link network="vk" url="" class="icon">
+                        <vk></vk>
+                    </share-link>
+                </li>
+                <li>
+                    <share-link network="ok" url="" class="icon">
+                        <ok></ok>
+                    </share-link>
+                </li>
+                <li>
+                    <share-link network="tw" url="" title="" class="icon">
+                        <tw></tw>
+                    </share-link>
+                </li>
+            </ul>
+
+
+        </div>
+    </Resize>
+</template>
+
+<script>
+
+    import Resize from '@/components/common/Resize';
+    import Menu from '@/components/common/Menu';
+
+    import icons from '@/assets/svg/social';
+
+    import vk from '@/assets/svg/social/vk.svg';
+
+    export default {
+        name: 'app',
+        components: {
+            vk: icons.vk,
+            fb: icons.fb,
+            ok: icons.ok,
+            googleplus: icons.googleplus,
+            tw: icons.tw,
+            'menu-app': Menu,
+            Resize
+        }
+    };
+</script>
+
+<style lang="scss">
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+    }
+    .logo{
+        margin-top: 60px;
+    }
+    .social-list  {
+        text-align: center;
+        li{
+            width: 1.5em;
+            height: 1.5em;
+            margin: 1em .5em;
+            display: inline-block;
+        }
+    }
+</style>
